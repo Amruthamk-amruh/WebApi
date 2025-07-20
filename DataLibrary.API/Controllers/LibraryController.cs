aab40219a5d529b2515fa8c962b6704537b85852
Amrutha M K <mkamruthakrishnan@gmail.com>
2025-07-20 14:33:53 +0530

    Add project files.

---
+++ DataLibrary.API/Controllers/LibraryController.cs
@@ -0,0 +1,21 @@
+﻿using DataLibrary.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataLibrary.API.Controllers
+{
+	//[Route("api/[Controller]")]
+	[Route("api/Library")]
+	[ApiController]
+	public class LibraryController : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<Library> GetLibraries()
+		{
+			return new List<Library>()
+			{
+				new Library { Id = 1, Name = "Wings of Fire" },
+				new Library { Id = 2, Name = "Aadujeevitham" }
+			};
+		}
+	}
+}  
---
+++ DataLibrary.API/Models/Library.cs
@@ -0,0 +1,8 @@
+﻿namespace DataLibrary.API.Models
+{
+	public class Library
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
